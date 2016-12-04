ad0ec776646908c8e00ef874f4a446419bb6e296
Steven Pham <steven.pham.8.2000@gmail.com>
2016-12-03 22:44:53 -0500

    Added ability for camera to scale sprites based on corresponding hitbox in relation to a predefined 'viewbox'

--- include/rendering.hpp
+++ include/rendering.hpp
@@ -1,14 +1,19 @@
 #pragma once
 #include "gameLogic.hpp"
+#include <SFML/Main.hpp>
 #include <SFML/Graphics.hpp>
 
 namespace Rendering {
     class Camera {
-        Game::Vector pos;
+        Game::Rect viewBox;
     public:
-        void move(Game::Vector moveBy);
-        void setPos(Game::Vector newPos);
-        sf::Vector2<float> translate(Game::Vector gameCoords);
+        void move(const Game::Vector& moveBy);
+        void setPos(const Game::Vector& newPos);
+        void setSize(const Game::Vector& newSize);
+        void setViewBox(const Game::Rect& newViewBox);
+        void centerOn(const Game::Vector& centeringOn, const sf::Window& window);
+        sf::Vector2<float> translate(const Game::Vector& gameCoords);
+        sf::Vector2<float> scaleSpriteToMatcHitbox(const sf::Sprite& sprite, const sf::Window& window, const Game::Rect& hitbox);
     };
 
     sf::Vector2<float> scaleSpriteRelativeToWindow(const sf::Sprite& sprite, const sf::Window& window, const sf::Vector2<float>& size);
--- src/rendering.cpp
+++ src/rendering.cpp
@@ -4,17 +4,34 @@
 
 namespace Rendering {
 
-    void Camera::move(Game::Vector moveBy) {
-        pos.x += moveBy.x;
-        pos.y += moveBy.y;
+    void Camera::move(const Game::Vector& moveBy) {
+        viewBox.topLeft.x += moveBy.x;
+        viewBox.topLeft.y += moveBy.y;
     }
 
-    void Camera::setPos(Game::Vector newPos) {
-        pos = newPos;
+    void Camera::setPos(const Game::Vector& newPos) {
+        viewBox.topLeft = newPos;
     }
 
-    sf::Vector2<float> Camera::translate(Game::Vector gameCoords) {
-        return sf::Vector2<float>(gameCoords.x - pos.x, gameCoords.y - pos.y);
+    void Camera::setSize(const Game::Vector& newSize) {
+        viewBox.width = newSize.x;
+        viewBox.height = newSize.y;
+    }
+
+    void Camera::setViewBox(const Game::Rect& newViewBox) {
+        viewBox = newViewBox;
+    }
+
+    sf::Vector2<float> Camera::translate(const Game::Vector& gameCoords) {
+        return sf::Vector2<float>(gameCoords.x - viewBox.topLeft.x, gameCoords.y - viewBox.topLeft.y);
+    }
+
+    void Camera::centerOn(const Game::Vector& centeringOn, const sf::Window& window) {
+        setPos(Game::Vector(centeringOn.x - window.getSize().x / 2, centeringOn.y - window.getSize().y / 2));
+    }
+
+    sf::Vector2<float> Camera::scaleSpriteToMatcHitbox(const sf::Sprite& sprite, const sf::Window& window, const Game::Rect& hitbox) {
+        scaleSpriteRelativeToWindow(sprite, window, sf::Vector2<float>((float)hitbox.width / (float)viewBox.width, (float)hitbox.height / (float)viewBox.height));
     }
 
     sf::Vector2<float> scaleSpriteRelativeToWindow(const sf::Sprite& sprite, const sf::Window& window, const sf::Vector2<float>& size) {
