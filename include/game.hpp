7b40ac9ec665461d765b1fbf8cd591c936046d03
Steven Pham <steven.pham.8.2000@gmail.com>
2016-12-08 15:53:00 -0500

    Added header file containting main game instance

---
+++ include/game.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include "gameLogic.hpp"
+#include "rendering.hpp"
+#include "io.hpp"
+#include <SFML/Main.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+namespace Main {
+    class Game {
+
+    public:
+        void run();
+    };
+}
