844987316c69c7c62a9e0b34e86bf295f26261a1
Steven Pham <steven.pham.8.2000@gmail.com>
2016-12-09 10:12:55 -0500

    Added member function 'run' and member variables to Game class

--- include/game.hpp
+++ include/game.hpp
@@ -11,9 +11,17 @@
 #include <SFML/Graphics.hpp>
 
 namespace Main {
-    class Game {
 
+    class Game {
+        Game::Map map;
+        sf::RenderWindow window;
+        std::vector<sf::Texture> textures;
+        std::vector<sf::Sprites> sprites;
+        Rendering::Camera camera;
+        sf::Keyboard keyboard;
+        std::vector<KeyHandler*> keyHandlers;
     public:
         void run();
     };
+
 }
