10a0e9f9228ef3440207ffc46ab8613bd3e770a3
Steven Pham <steven.pham.8.2000@gmail.com>
2017-01-01 22:20:52 -0500

    Added subclass of KeyHandler, MouseHandler for handling mouse events

--- include/io.hpp
+++ include/io.hpp
@@ -7,11 +7,18 @@
 #include <SFML/Window.hpp>
 
 namespace IO {
+
     class KeyHandler {
     protected:
         virtual void onKeyPress(sf::Keyboard::Key pressed)=0;
     public:
         virtual void checkForKeyPress()=0;
+    };
+
+    class MouseHandler {
+    protected:
+        virtual void onMouseButtonPress(sf::Mouse::Button pressed)=0;
+        virtual void onMouseMove(sf::Vector2<int> position)=0;
     };
 
     class CameraKeyHandler : public KeyHandler {
